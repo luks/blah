5d8a4f23fed9ac55b4610a2a63e235922a4d0496
Luky <lukapiske@gmail.com>
2015-10-12 00:50:53 +0200

    Regex match functional but memory liks ocur somewhere sometimes TODO Fix: noncompiled regex, empty inputs

--- ext/blah/blah.c
+++ ext/blah/blah.c
@@ -1,10 +1,87 @@
 #include <ruby.h>
-#include <stdio.h>
+#include <stdio.h> //remove latter
+#include <regex.h>
+#include <string.h> //strlen
+#include <stdlib.h> //malloc, memcpy
 
+/*
+  header
+*/
 typedef struct {
     int numerator, denominator;
     double value;
 } ratio_s;
+
+
+typedef struct {
+    const char *string;
+    const char *regex;
+    char ***substrings;
+    _Bool use_case;
+} regex_fn_s;
+
+
+#define regex_match(...) regex_match_base((regex_fn_s){__VA_ARGS__})
+int regex_match_base(regex_fn_s in);
+
+/*
+  end of header
+*/
+
+
+/*
+  regex
+*/
+static int count_parens(const char *string){
+    int out = 0;
+    int last_was_backslash = 0;
+    for(const char *step=string; *step !='\0'; step++){
+        if (*step == '\\' && !last_was_backslash){
+            last_was_backslash = 1;
+            continue;
+        }
+        if (*step == ')' && !last_was_backslash)
+            out++;
+        last_was_backslash = 0;
+    }
+    return out;
+}
+int regex_match_base(regex_fn_s in){
+    regex_t re;
+    int matchcount = 0;
+    if (in.substrings) matchcount = count_parens(in.regex);
+    regmatch_t result[matchcount+1];
+    int compiled_ok = !regcomp(&re, in.regex, REG_EXTENDED
+                                            + (in.use_case ? 0 : REG_ICASE)
+                                            + (in.substrings ? 0 : REG_NOSUB) );
+
+    int found = !regexec(&re, in.string, matchcount+1, result, 0);
+    if (!found) return 0;
+    if (in.substrings){
+         *in.substrings = malloc(sizeof(char*) * matchcount);
+         char **substrings = *in.substrings;
+        //match zero is the whole string; ignore it.
+        for (int i=0; i< matchcount; i++){
+            if (result[i+1].rm_eo > 0){//GNU peculiarity: match-to-empty marked with -1.
+                int length_of_match = result[i+1].rm_eo - result[i+1].rm_so;
+                substrings[i] = malloc(strlen(in.string)+1);
+                memcpy(substrings[i], in.string + result[i+1].rm_so, length_of_match);
+                substrings[i][length_of_match] = '\0';
+            } else { //empty match
+                substrings[i] = malloc(1);
+                substrings[i][0] = '\0';
+            }
+        }
+        in.string += result[0].rm_eo; //end of whole match;
+    }
+    regfree(&re);
+    return matchcount;
+}
+
+/*
+  ration
+*/
+
 
 ratio_s new_ratio(int num, int den){
     return (ratio_s){.numerator=num, .denominator=den, .value=num/(double)den};
@@ -17,6 +94,25 @@ ratio_s ratio_add(ratio_s left, ratio_s right){
         .denominator=left.denominator * right.denominator,
         .value=left.value + right.value
     };
+}
+
+static VALUE c_match;
+
+static VALUE match(VALUE self, VALUE string, VALUE regex)
+{
+
+  char **substrings;
+  int match_ct =  regex_match(RSTRING_PTR(string), RSTRING_PTR(regex), &substrings);
+  if(match_ct != 0) {
+    printf("%i matches:\n", match_ct);
+    for (int i=0; i< match_ct; i++){
+        printf("[%s] ", substrings[i]);
+        free(substrings[i]);
+    }
+    free(substrings);
+    printf("\n\n");
+  }
+  return Qnil;
 }
 
 static VALUE print_ratio(VALUE self, VALUE num, VALUE den)
@@ -49,8 +145,12 @@ static VALUE goodbye(VALUE class) {
 void Init_blah()
 {
   VALUE mBlah = rb_define_module("Blah");
+  c_match = rb_define_class_under(mBlah, "Match", rb_cObject);
+  //rb_define_method(c_match, "string", match_string, 0);
+
   rb_define_singleton_method(mBlah, "hello_world", hello_world, 0);
   rb_define_singleton_method(mBlah, "print_ratio", print_ratio, 2);
+  rb_define_singleton_method(mBlah, "match", match, 2);
   rb_define_method(mBlah, "hello_world", hello_world, 0);
   rb_define_method(mBlah, "hello", hello, 1);
   rb_define_method(mBlah, "goodbye", goodbye, 0);
